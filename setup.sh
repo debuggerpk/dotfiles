b6e718e92522b2fe6bbedc2531298dddfa6a589b
Yousuf Jawwad <yousuf@maher.pk>
2020-03-21 16:44:37 -0700

    eat(install): installation script for dotfiles
    clones the github repo at $HOME/dotfiles
    installs homebrew (https://brew.sh)
    installs zplug (https://github.com/zplug/zplug)
    installs various tools (via homebrew and homebrew casks)
    
    TODO: add `brew mas` (mac app store) scripts for xcode

--- setup.sh
+++ setup.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
+# install brew
+/bin/bash <(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)
+# install & activate zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+source ~/.zplug/init.zsh
+
+# clone dotfiles to $HOME/dotfiles
+git clone git@github.com:getbreu/dotfiles.git ~/dotfiles
+
+# Backup Existing .zshrc, and create a link to ~/dotfiles/.zshrc in $HOME
+[ -s "${HOME}/.zshrc" ] && mv ${HOME}/.zshrc ${HOME}/.zshrc.bck
+ln -s ${HOME}/dotfiles/.zshrc ${HOME}/.zshrc
+
+# install all packages via brew
+CURRENT_DIR=`pwd`
 export CLOUDSDK_PYTHON='/usr/bin/python3'
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+cd ${HOME}/dotfiles
+brew bundle
+cd ${CURRENT_DIR}
